3e9f6c421bbeed4c1e817da0702b9bc0bd86b1cb
arun.krg <40122909+arungautam071@users.noreply.github.com>
2022-10-18 21:02:34 +0530

    code commit

---
+++ employee_database_code.py
@@ -0,0 +1,31 @@
+#django model database code for Employee-Department-Project relationship
+
+from django.db import models
+
+
+
+class Department(models.Model):
+    D_number = models.IntegerField(max_length=30)
+    D_name = models.CharField(max_length=30)
+    D_location = models.CharField()
+
+    
+
+class Employee(models.Model):
+    e_name = models.CharField(max_length=20)
+    e_gender = models.CharField(max_length=10)
+    e_dob = models.DateTimeField()
+    e_doj = models.DateTimeField()
+    e_adder = models.CharField(max_length=100)
+    e_department= models.ForeignKey(Department, on_delete=models.CASCADE)
+
+
+
+class Project(models.Model):
+    p_number = models.IntegerField(max_length=30)
+    p_name = models.CharField(max_length=30)
+    p_location = models.CharField()
+    p_department= models.ForeignKey(Department, on_delete=models.CASCADE)
+    p_employee =models.ManyToManyField(Employee)    
+
+   
